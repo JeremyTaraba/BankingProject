b2f0c259bf6bb013ecedd87e5a57845c320ba941
Jeremy Taraba <jeremytaraba@gmail.com>
2022-03-31 19:54:14 -0700

    Update Banking.java

--- Banking.java
+++ Banking.java
@@ -4,83 +4,79 @@ import java.util.*;
 public class Banking{
 
 
-    public static void login(Scanner input, Hashtable<String, String> logins){
+    public static String login(Hashtable<String, String> logins, Scanner input){
         //should put login stuff in its own function. so can loop over it easy if incorrect username/password
-        String user;
+        String user = "";
         String pass;
-        boolean failedLogin = true;
+        boolean failLogin = true;
 
-        
-        
-        while(failedLogin){
-            System.out.println("Enter Your Username:");
+
+
+        while(failLogin)
+        {
+            System.out.println("Enter your username: ");
             user = input.nextLine();
-            System.out.println("Enter Your Password:");
+            System.out.println("Enter your password: ");
             pass = input.nextLine();
-            
-            if (logins.containsKey(user)) {
-                // Okay, there's a key
-                if (logins.get(user).equals(pass)) {    
-                    //value equals password
-                    System.out.println("Login Successful!");
-                    failedLogin = false;
+            if (logins.containsKey(user))
+            {
+                if (logins.get(user).equals(pass))
+                {
+                    System.out.println("login succesful");
+                    failLogin = false;
                 }
-                else{
-                    //Username exists but wrong Password
-                    System.out.println("Incorrect Username/Password. Try Again");
+                else
+                {
+                    System.out.println("Incorrect Username or Password");
                 }
-            } else {
-                // Definitely no such key
-                System.out.println("Incorrect Username/Password. Try Again");
             }
-
+            else
+            {
+                System.out.println("Incorrect Username or Password");
+            }
         }
-
         
-
+       return user;
     }
 
     public static void createAccount(){
-        //check if username is already taken before accepting their account. Store their username and password into the txt file of logins.
+        //should put login stuff in its own function. so can loop over it easy if incorrect username/password
     }
 
     public static int start(Scanner input){
-        //Scanner input = new Scanner(System.in); // can't have a scanner here because when we close it, it also closes any inputs from system.in
+       
         boolean validChoice = true;
-        int choiceInt = -1;
 
         System.out.println("Welcome to the bank. Enter 1. for Login and 2. for New Account 3. for Quit");
-        String choice;
-        
+        String choice = input.nextLine();
+       
         while(validChoice){
-            choice = input.nextLine();
-            validChoice = false;
+            validChoice = true;
             if(choice.equals("1")){
-                choiceInt = 1;
+                return 1;
             }
             else if(choice.equals("2")){
-                choiceInt = 2;
+                return 2;
             }
             else if(choice.equals("3")){
-                choiceInt = 3;
+                System.exit(0);
             }
             else{
                 //loop back
                 System.out.println("Not a Valid Choice");
-                validChoice = true;
-                System.out.println("Choose Again");
+                validChoice = false;
             }
         }
         
-        
-        return choiceInt;
+        return 0;
     }
 
 
     public static void main(String args[]) throws Exception { 
         File file = new File("C:\\Users\\jghos\\OneDrive\\Documents\\Code\\Java\\login.txt");
         Scanner sc = new Scanner(file);
-        Scanner input = new Scanner(System.in); 
+        Scanner input = new Scanner(System.in);
+        String username = "";
         
 
         Hashtable<String, String> logins = new Hashtable<String, String>();
@@ -89,36 +85,33 @@ public class Banking{
         while(sc.hasNextLine()){
             logins.put(sc.nextLine(), sc.nextLine());
         }
-        sc.close();
 
         int startChoice = start(input);
 
-
         if(startChoice == 1){
-            login(input, logins);   //returns on successful login
-            //create bank object using login info, retrieve things like account balance, bills, id, stuff like that.
-            //once in their account we can have options like withdrawl and deposit and stuff.
+            username = login(logins, input);
         }
         else if(startChoice == 2){
             createAccount();
-            //after account is successfully created, let them login, can also do start menu stuff again.
-        }
-        else if(startChoice == 3){
-            return;
         }
         else{
-            System.out.println("Should not reach here, something is wrong");
-            return;
+            //pass
         }
 
-// ideas: add something for when user/password is incorrect and maybe add limited attempts to login like 5 and then they have to wait to try again
+        
 
 
+
+
+        // ideas: add something for when user/password is incorrect and maybe add limited attempts to login like 5 and then they have to wait to try again
+
+
+    
 
         
 
         input.close();
-
+        sc.close();
     }
 
     
